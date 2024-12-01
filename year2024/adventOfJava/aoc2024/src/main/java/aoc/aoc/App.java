408a78d3eb94e0100ed718c5125e79b9fead984b
Miika Tiihonen <m95.tiihonen@gmail.com>
2024-12-01 13:00:55 +0200

    initial

---
+++ year2024/adventOfJava/aoc2024/src/main/java/aoc/aoc/App.java
@@ -0,0 +1,13 @@
+package aoc.aoc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
