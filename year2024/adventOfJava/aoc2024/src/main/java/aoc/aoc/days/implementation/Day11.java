84efc27c06880a9e38bbfe1925f202b0ab2b8163
Miika Tiihonen <m95.tiihonen@gmail.com>
2024-12-28 22:53:13 +0200

    Day11 iterative faster than recursion

--- year2024/adventOfJava/aoc2024/src/main/java/aoc/aoc/days/implementation/Day11.java
+++ year2024/adventOfJava/aoc2024/src/main/java/aoc/aoc/days/implementation/Day11.java
@@ -3,46 +3,65 @@ package aoc.aoc.days.implementation;
 import aoc.aoc.cache.Memoize;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
-import static aoc.aoc.util.Utils.arrayOf;
-import static aoc.aoc.util.Utils.isEven;
+import static aoc.aoc.util.Utils.*;
 
 public class Day11 extends AbstractDay {
 
     @Override
     protected String part1Impl(String input) {
-        return "" + countStonesAfterBlinks(input, 25);
+        return "" + countStonesAfterBlinksIterative(input, 25);
     }
 
     @Override
     protected String part2Impl(String input) {
-        return "" + countStonesAfterBlinks(input, 75);
+        return "" + countStonesAfterBlinksIterative(input, 75);
     }
 
     private static final long MULTIPLIER = 2024L;
 
-    private long countStonesAfterBlinks(String input, int blinks) {
+    private long countStonesAfterBlinksIterative(String input, int blinks) {
+        Map<Long, Long> stones = streamStones(input).boxed()
+                .collect(Collectors.toMap(__ -> __, __ -> 1L, Long::sum));
+
+        for (int i = 0; i < blinks; i++) {
+            Map<Long, Long> nextLevel = new HashMap<>();
+
+            stones.forEach((stone, accumulated) -> {
+                for (long s : resultingStonesAfterBlink(stone))
+                    nextLevel.merge(s, accumulated, Long::sum);
+            });
+
+            stones = nextLevel;
+        }
+
+        return stones.values().stream()
+                .mapToLong(Long::longValue).sum();
+    }
+
+    @SuppressWarnings("unused")
+    private long countStonesAfterBlinksRecursive(String input, int blinks) {
         return streamStones(input)
-                .map(stone -> processStone(stone, blinks))
+                .map(stone -> processStoneRecursive(stone, blinks))
                 .sum();
     }
 
     @Memoize
-    protected long processStone(long stone, int depth) {
+    protected long processStoneRecursive(long stone, int depth) {
         if (depth <= 0)
             return 1L;
 
         long accumulated = 0;
         for (var s : resultingStonesAfterBlink(stone)) {
-            accumulated += processStone(s, depth - 1);
+            accumulated += processStoneRecursive(s, depth - 1);
         }
 
         return accumulated;
     }
 
     @SuppressWarnings("java:S1121")
-    @Memoize
     protected long[] resultingStonesAfterBlink(long stone) {
         int stoneLength;
         if (stone == 0)
