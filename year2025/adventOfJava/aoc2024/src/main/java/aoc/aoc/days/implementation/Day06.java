2b4da6a750d739cd69dc34738c7568013bba1ce0
Miika Tiihonen <m95.tiihonen@gmail.com>
2025-12-07 00:59:45 +0200

    day06 generalized solution

--- year2025/adventOfJava/aoc2024/src/main/java/aoc/aoc/days/implementation/Day06.java
+++ year2025/adventOfJava/aoc2024/src/main/java/aoc/aoc/days/implementation/Day06.java
@@ -1,92 +1,102 @@
 package aoc.aoc.days.implementation;
 
-import aoc.aoc.days.interfaces.DayCharacterArrayParser;
+import aoc.aoc.days.enums.Part;
+import aoc.aoc.days.interfaces.DayStringParser;
 import aoc.aoc.util.GenericMatrix;
 import aoc.aoc.util.Operand;
-import aoc.aoc.util.StringMatrix;
-import aoc.aoc.util.Utils;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class Day06 extends DayCharacterArrayParser {
+import static aoc.aoc.days.enums.Part.*;
+
+public class Day06 extends DayStringParser {
 
     @Override
-    protected String part1Impl(char[] input) {
-        var temp = new String(input).lines()
-                .map(line -> line.trim().split("\\s+"))
-                .toList();
-        List<List<Integer>> numbers = new ArrayList<>();
-        for (int i = 0; i < temp.size() - 1; i++) {
-            var line = temp.get(i);
-            numbers.add(Arrays.stream(line).map(Integer::parseInt).toList());
-        }
-        var operands = Arrays.stream(temp.getLast())
-                .map(Operand::fromString)
-                .toList();
-
-        long sum = 0L;
-        for (int x = 0; x < numbers.getFirst().size(); x++) {
-            var operand = operands.get(x);
-            long current = numbers.getFirst().get(x);
-            for (int y = 1; y < numbers.size(); y++) {
-                int next = numbers.get(y).get(x);
-                switch (operand) {
-                    case ADD -> current += next;
-                    case SUB -> current -= next;
-                    case MUL -> current *= next;
-                    case DIV -> current /= next;
-                }
-            }
-            sum += current;
-        }
-
-        return "" + sum;
+    protected String part1Impl(String input) {
+        return solveMathHomework(input);
     }
 
     @Override
-    protected String part2Impl(char[] input) {
-        var lines = new String(input).lines().toList();
-        int widestRow = 0;
-        for (String line : lines) {
-            if (line.length() > widestRow)
-                widestRow = line.length();
+    protected String part2Impl(String input) {
+        return solveMathHomework(input);
+    }
+
+    private record Region(GenericMatrix<Character> numbers, Operand operand) {
+        long calculate(long a, long b) {
+            return switch (operand) {
+                case ADD -> a + b;
+                case MUL -> Math.max(1L, a) * b;
+                default -> throw new IllegalStateException("Unexpected value: " + operand);
+            };
+        }
+    }
+
+    private String solveMathHomework(String input) {
+        var lines = input.lines().collect(Collectors.toCollection(ArrayList::new));
+        long result = 0L;
+        for (var region : buildRegions(lines, this.part))
+            result += calculateRegion(region);
+
+        return "" + result;
+    }
+
+    private static long calculateRegion(Region region) {
+        var matrix = region.numbers;
+
+        long total = 0L;
+        for (int y = 0; y < matrix.height(); y++) {
+            long current = 0L;
+            for (int x = 0; x < matrix.width(); x++) {
+                char ch = matrix.get(y, x);
+                if (ch != ' ')
+                    current = current * 10 + (ch - '0');
+            }
+            
+            total = region.calculate(total, current);
         }
 
-        var numbers = new ArrayList<Integer>();
-        long sum = 0L;
-        for (int col = widestRow - 1; col >= -1; col--) {
-            var sb = new StringBuilder();
-            for (int row = 0; row < lines.size() - 1; row++) {
-                try {
-                    sb.append(lines.get(row).charAt(col));
-                } catch (IndexOutOfBoundsException e) {
-                    // dont care
-                }
-            }
-            if (StringUtils.isBlank(sb)) {
-                var operand = Operand.fromChar(lines.getLast().charAt(col + 1));
-                long current = numbers.getFirst();
-                for (int i = 1; i < numbers.size(); i++) {
-                    int next = numbers.get(i);
-                    switch (operand) {
-                        case ADD -> current += next;
-                        case SUB -> current -= next;
-                        case MUL -> current *= next;
-                        case DIV -> current /= next;
-                    }
-                }
-                sum += current;
-                numbers.clear();
-            } else {
-                numbers.add(Integer.parseInt(sb.toString().trim()));
-            }
+        return total;
+    }
+
+    private static ArrayList<Region> buildRegions(ArrayList<String> lines, Part part) {
+        var operands = lines.removeLast();
+        var regions = new ArrayList<Region>();
+
+        int start = operands.length();
+        int end = operands.length();
+        while (--start >= 0) {
+            var ch = operands.charAt(start);
+            if (!isOperand(ch))
+                continue;
+
+            regions.add(new Region(
+                    buildRegionMatrix(lines, start, end, part), Operand.fromChar(ch)
+            ));
+
+            end = start - 1;
         }
 
-        return "" + sum;
+        return regions;
+    }
+
+    private static GenericMatrix<Character> buildRegionMatrix(
+            List<String> lines, int start, int end, Part part
+    ) {
+        var region = new ArrayList<List<Character>>();
+
+        for (var line : lines) {
+            var row = new ArrayList<Character>();
+            for (int j = start; j < end; j++)
+                row.add(line.charAt(j));
+            region.add(row);
+        }
+
+        var matrix = new GenericMatrix<>(region);
+        return part == PART_1 ? matrix : matrix.rotate90AntiClockwise();
+    }
+
+    private static boolean isOperand(char ch) {
+        return ch == '*' || ch == '+';
     }
 }
