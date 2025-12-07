3520c3b350619026d10f97b31a70f99ff413017c
Miika Tiihonen <m95.tiihonen@gmail.com>
2025-12-07 14:56:29 +0200

    day07 clean-up

--- year2025/adventOfJava/aoc2024/src/main/java/aoc/aoc/days/implementation/Day07.java
+++ year2025/adventOfJava/aoc2024/src/main/java/aoc/aoc/days/implementation/Day07.java
@@ -4,53 +4,58 @@ import aoc.aoc.cache.Memoize;
 import aoc.aoc.days.interfaces.DayStringParser;
 import aoc.aoc.util.Coordinate;
 import aoc.aoc.util.GenericMatrix;
+import aoc.aoc.util.Matrix;
 
 import java.util.Set;
 
-public class Day07 extends DayStringParser {
+import static aoc.aoc.util.GenericMatrix.charMatrix;
+import static aoc.aoc.util.MatrixUtils.*;
 
+public class Day07 extends DayStringParser {
+    
     @Override
     protected String part1Impl(String input) {
-        var set = goDownUntilNode(GenericMatrix.charMatrix(input), 0, input.indexOf('S'));
+        var set = p1(charMatrix(input), 0, input.indexOf('S'));
         return "" + set.size();
     }
 
     @Override
     protected String part2Impl(String input) {
-        return "" + (goDownUntilNode2(GenericMatrix.charMatrix(input), 0, input.indexOf('S')) + 1);
+        return "" + (p2(charMatrix(input), 0, input.indexOf('S')) + 1);
+    }
+    
+    @Memoize
+    protected long p2(Matrix<Character> matrix, int y, int x) {
+        if (notWithinMatrix(y, x, matrix))
+            return 0L;
+
+        while (notEndOrSplitter(matrix, y, x))
+            ++y;
+
+        return y == matrix.height() ? 0L 
+                : 1L 
+                + p2(matrix, y, x - 1) 
+                + p2(matrix, y, x + 1);
     }
 
     @Memoize
-    protected long goDownUntilNode2(GenericMatrix<Character> matrix, int y, int x) {
-        long res = 0;
-        try {
-            while (matrix.get(y, x) != '^')
-                ++y;
-
-            ++res;
-            res += (goDownUntilNode2(matrix, y, x - 1));
-            res += (goDownUntilNode2(matrix, y, x + 1));
-        } catch (Exception e) {
-            // don't care
-        }
-
-        return res;
-    }
-
-    @Memoize
-    protected Set<Coordinate> goDownUntilNode(GenericMatrix<Character> matrix, int y, int x) {
+    protected Set<Coordinate> p1(GenericMatrix<Character> matrix, int y, int x) {
         var set = new java.util.HashSet<Coordinate>();
         try {
             while (matrix.get(y, x) != '^')
                 ++y;
 
             set.add(new Coordinate(y, x));
-            set.addAll(goDownUntilNode(matrix, y, x - 1));
-            set.addAll(goDownUntilNode(matrix, y, x + 1));
+            set.addAll(p1(matrix, y, x - 1));
+            set.addAll(p1(matrix, y, x + 1));
         } catch (Exception e) {
             // don't care
         }
 
         return set;
+    }
+
+    private static boolean notEndOrSplitter(Matrix<Character> matrix, int y, int x) {
+        return y < matrix.height() && matrix.get(y, x) != '^';
     }
 }
